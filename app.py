71358cabceefcf8612052bf57683b5d6dbc051be
Lamar <123819924+0xLully@users.noreply.github.com>
2025-11-08 16:59:54 +0300

    Change app.run to bind to all interfaces

--- app.py
+++ app.py
@@ -61,6 +61,5 @@ def info():
     """
 
 # تشغيل التطبيق
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
